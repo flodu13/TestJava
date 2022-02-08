6a05ea4b13e3c293485aba70f79d6961cb8f5f0d
Florent Ros <ros-florent@hotmail.fr>
2022-02-08 12:39:20 +0100

    Initial commit

---
+++ src/myerp-consumer/src/main/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/SequenceEcritureComptableRM.java
@@ -0,0 +1,26 @@
+package com.dummy.myerp.consumer.dao.impl.db.rowmapper.comptabilite;
+
+import com.dummy.myerp.consumer.dao.impl.cache.CompteComptableDaoCache;
+import com.dummy.myerp.model.bean.comptabilite.LigneEcritureComptable;
+import com.dummy.myerp.model.bean.comptabilite.SequenceEcritureComptable;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+/**
+ * {@link RowMapper} de {@link SequenceEcritureComptable}
+ */
+public class SequenceEcritureComptableRM implements RowMapper<SequenceEcritureComptable> {
+
+    @Override
+    public SequenceEcritureComptable mapRow(ResultSet pRS, int pRowNum) throws SQLException {
+        SequenceEcritureComptable vBean = new SequenceEcritureComptable();
+
+        vBean.setAnnee(pRS.getInt("annee"));
+        vBean.setDerniereValeur(pRS.getInt("derniereValeur"));
+
+        return vBean;
+    }
+}
